xiaomi-sm8450-marble/android_hardware_dolby

dolby: Switch to DolbyManager
Based on existing dirac implementation and observing stock
sound effects app and daxservice.

History -
- Allow bass enhancer to use in speaker also
- Remove deprecated PlainTooltipBox
- Fix build with kotlinc 1.9.0
- Add launcher icon
- Introduce graphical equalizer
- Do not set volume leveler amount
- Restore all settings upon bootup
- Rewrite in Kotlin
- Restore dolby profile on audio changes
- Guard debug logging
- Implement profile-specific Dolby settings
- Remove play/pause hack while toggling Dolby
- Set proper summary for dolby settings
- Always refresh playback if status changed
- Introduce Dolby Atmos

Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
Co-authored-by: basamaryan <basam.aryan@gmail.com>
Co-authored-by: Ghosuto <clash.raja10@gmail.com>

DolbyManager/src/co/aospa/dolby/BootCompletedReceiver.kt
@@ -1,0 +1,27 @@
+/*
+ * Copyright (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+private const val TAG = "XiaomiDolby-Boot"
+
+class BootCompletedReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Received intent: ${intent.action}")
+        if (intent.action != Intent.ACTION_BOOT_COMPLETED) {
+            return
+        }
+
+        Log.i(TAG, "Boot completed, starting dolby")
+        DolbyController.getInstance(context).onBootCompleted()
+    }
+}

DolbyManager/src/co/aospa/dolby/DolbyConstants.kt
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby
+
+import android.util.Log
+
+class DolbyConstants {
+
+    enum class DsParam(val id: Int, val length: Int = 1) {
+        HEADPHONE_VIRTUALIZER(101),
+        SPEAKER_VIRTUALIZER(102),
+        VOLUME_LEVELER_ENABLE(103),
+        DIALOGUE_ENHANCER_ENABLE(105),
+        DIALOGUE_ENHANCER_AMOUNT(108),
+        GEQ_BAND_GAINS(110, 20),
+        BASS_ENHANCER_ENABLE(111),
+        STEREO_WIDENING_AMOUNT(113);
+
+        override fun toString(): String {
+            return "${name}(${id})"
+        }
+    }
+
+    companion object {
+        const val TAG = "XiaomiDolby"
+        const val PREF_ENABLE = "dolby_enable"
+        const val PREF_PROFILE = "dolby_profile"
+        const val PREF_PRESET = "dolby_preset"
+        const val PREF_HP_VIRTUALIZER = "dolby_virtualizer"
+        const val PREF_SPK_VIRTUALIZER = "dolby_spk_virtualizer"
+        const val PREF_STEREO = "dolby_stereo"
+        const val PREF_DIALOGUE = "dolby_dialogue"
+        const val PREF_BASS = "dolby_bass"
+        const val PREF_VOLUME = "dolby_volume"
+        const val PREF_RESET = "dolby_reset"
+
+        val PROFILE_SPECIFIC_PREFS = setOf(
+            PREF_PRESET,
+            PREF_HP_VIRTUALIZER,
+            PREF_SPK_VIRTUALIZER,
+            PREF_STEREO,
+            PREF_DIALOGUE,
+            PREF_BASS,
+            PREF_VOLUME
+        )
+
+        fun dlog(tag: String, msg: String) {
+            if (Log.isLoggable(TAG, Log.DEBUG)) {
+                Log.d(tag, msg)
+            }
+        }
+    }
+}

sm8635-dev/vendor_sony_dolby

DolbyManager/src/co/aospa/dolby/BootCompletedReceiver.kt
@@ -1,0 +1,27 @@
+/*
+ * Copyright (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+private const val TAG = "Dolby-Boot"
+
+class BootCompletedReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Received intent: ${intent.action}")
+        if (intent.action != Intent.ACTION_BOOT_COMPLETED) {
+            return
+        }
+
+        Log.i(TAG, "Boot completed, starting dolby")
+        DolbyController.getInstance(context).onBootCompleted()
+    }
+}

DolbyManager/src/co/aospa/dolby/DolbyConstants.kt
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby
+
+import android.util.Log
+
+class DolbyConstants {
+
+    enum class DsParam(val id: Int, val length: Int = 1) {
+        HEADPHONE_VIRTUALIZER(101),
+        SPEAKER_VIRTUALIZER(102),
+        VOLUME_LEVELER_ENABLE(103),
+        DIALOGUE_ENHANCER_ENABLE(105),
+        DIALOGUE_ENHANCER_AMOUNT(108),
+        GEQ_BAND_GAINS(110, 20),
+        BASS_ENHANCER_ENABLE(111),
+        STEREO_WIDENING_AMOUNT(113);
+
+        override fun toString(): String {
+            return "${name}(${id})"
+        }
+    }
+
+    companion object {
+        const val TAG = "Dolby"
+        const val PREF_ENABLE = "dolby_enable"
+        const val PREF_PROFILE = "dolby_profile"
+        const val PREF_PRESET = "dolby_preset"
+        const val PREF_HP_VIRTUALIZER = "dolby_virtualizer"
+        const val PREF_SPK_VIRTUALIZER = "dolby_spk_virtualizer"
+        const val PREF_STEREO = "dolby_stereo"
+        const val PREF_DIALOGUE = "dolby_dialogue"
+        const val PREF_BASS = "dolby_bass"
+        const val PREF_VOLUME = "dolby_volume"
+        const val PREF_RESET = "dolby_reset"
+
+        val PROFILE_SPECIFIC_PREFS = setOf(
+            PREF_PRESET,
+            PREF_HP_VIRTUALIZER,
+            PREF_SPK_VIRTUALIZER,
+            PREF_STEREO,
+            PREF_DIALOGUE,
+            PREF_BASS,
+            PREF_VOLUME
+        )
+
+        fun dlog(tag: String, msg: String) {
+            if (Log.isLoggable(TAG, Log.DEBUG)) {
+                Log.d(tag, msg)
+            }
+        }
+    }
+}